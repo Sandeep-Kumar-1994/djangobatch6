5e7eb2d642a9ae6023458a5f44275120963c280f
Sandeep-Kumar-1994 <sandeepvoices@gmail.com>
2018-07-09 08:56:53 +0530

    changes commited

--- Datacenter/settings.py
+++ Datacenter/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cooling2',
     'cooling',
+    'cooling2',
 ]
 
 MIDDLEWARE = [
